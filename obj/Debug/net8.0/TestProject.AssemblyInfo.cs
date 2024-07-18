8b60b696643af12b4cf98ebcc1ee6040a843d9db
Cotne Basiashvili <basiashvilic@gmail.com>
2024-07-18 09:51:42 +0400

    learned classes

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e29d45dec0d10b93b083796e93d3a974a63b3b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6e985822a836a34ae3d35a962d3c91a8a615777")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
