26bb293142f9184b579afd864396df6600e13bb6
Cotne Basiashvili <basiashvilic@gmail.com>
2024-07-22 18:08:50 +0400

    build some projects

--- Program.cs
+++ Program.cs
@@ -1,4 +1,59 @@
-﻿string permission = "Admin|Manager";
-int level = 19;
+﻿// Console.WriteLine("Enter an integer value between 5 and 10");
+// bool validNumber = false;
+// do
+// {
+//     string? userCode = Console.ReadLine();
+//     int numericValue;
+//     if (int.TryParse(userCode, out numericValue))
+//     {
+//         if (numericValue > 5 && numericValue < 10)
+//         {
+//             validNumber = true;
+//             Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+//         }
+//         else
+//         {
+//             Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//     }
+// } while (validNumber == false);
 
-Console.WriteLine(permission.Contains("Admin") && level > 55 ? "Welcome, Admin User" : permission.Contains("Manager") && level >= 20 ? "Contact an Admin for acces" : permission.Contains("Manager") && level < 20 ? "You do not have sufficient privileges" : !permission.Contains("Admin|Manager") ? "You do not have sufficient privileges" : "hehe");
+// Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+// bool validRole = false;
+// do
+// {
+//     string? role = Console.ReadLine();
+//     if (role != null)
+//         role = role.Trim().ToLower();
+
+//     {
+//         if (role == "administrator" || role == "manager" || role == "user")
+//         {
+//             Console.WriteLine($"Your input value ({role}) has been accepted.");
+//             validRole = true;
+//         }
+//         else
+//             Console.WriteLine($"The role name that you entered, \"{role}\" is not valid. Enter your role name (Administrator, Manager, or User) Administrator");
+//     }
+// } while (validRole == false);
+
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int periodLocation;
+foreach (string myString in myStrings)
+{
+    if (myString.IndexOf(".") != -1)
+    {
+        periodLocation = myString.IndexOf(".");
+        Console.WriteLine(myString.Substring(0, periodLocation));
+        Console.WriteLine(myString.Substring(periodLocation, myString.Length - periodLocation));
+        // do
+        // {
+        //     Console.WriteLine(myString.Substring(0, periodLocation));
+        // } while (false);
+    }
+}
--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b14c00edb58dc41ad1fe4db70df7b72647d5610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fd4f227be917a83a1acd62338da57f53c08024")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
