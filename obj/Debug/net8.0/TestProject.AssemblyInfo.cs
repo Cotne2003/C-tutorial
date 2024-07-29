182d9ee445f7858e1ea8f922c5ad4fcd9368925f
Tsotne Basiashvili <basiashvilic@gmail.com>
2024-07-29 21:00:59 +0400

    finished project searching methods

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a327ffb2d1fc6c9ccff470c866a7b9010e41471")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b55a7344e521c1bc66eac9045905884c7a5418e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
