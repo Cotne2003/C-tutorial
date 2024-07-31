66f0ec4893045a4550cc4f93f69dd229f45237f5
Tsotne Basiashvili <basiashvilic@gmail.com>
2024-07-31 05:44:26 +0400

    learned methods in C#

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b3341ef737972dbd44caaece807f5d2b1f6257e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061c65c0ce20b6cf165810f79f04f7643384cb89")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
