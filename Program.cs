1a3fd6ac1fbe2777460285de069f3cedff2d0083
Tsotne Basiashvili <basiashvilic@gmail.com>
2024-08-02 04:39:22 +0400

    finished project of mini-game of console

--- Program.cs
+++ Program.cs
@@ -1,68 +1,155 @@
 ﻿using System;
 
 Random random = new Random();
+Console.CursorVisible = false;
+int height = Console.WindowHeight - 1;
+int width = Console.WindowWidth - 5;
+bool shouldExit = false;
 
-string[] pettingZoo =
+// Console position of the player
+int playerX = 0;
+int playerY = 0;
+
+// Console position of the food
+int foodX = 0;
+int foodY = 0;
+
+// Available player and food strings
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
+
+// Current player string displayed in the Console
+string player = states[0];
+
+// Index of the current food
+int food = 0;
+
+InitializeGame();
+while (!shouldExit)
 {
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
-    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
-};
-
-Console.Clear();
-
-PlanSchoolVisit("School A");
-PlanSchoolVisit("School B", 3);
-PlanSchoolVisit("School C", 2);
-
-void RandomizeAnimals()
-{
-    Random random = new Random();
-
-    for (int i = 0; i < pettingZoo.Length; i++)
+    if (TerminalResized())
     {
-        int r = random.Next(i, pettingZoo.Length);
-
-        string temp = pettingZoo[i];
-        pettingZoo[i] = pettingZoo[r];
-        pettingZoo[r] = temp;
+        Console.Clear();
+        Console.WriteLine("Console was resized. Program exiting.");
+        shouldExit = true;
     }
+    if (GotFood())
+    {
+        ChangePlayer();
+        ShowFood();
+    }
+    if (TypeOfCharacter() == "freeze")
+    {
+        FreezePlayer();
+    }
+    else if (TypeOfCharacter() == "fast")
+    {
+        Move(3);
+    }
+    Move();
 }
 
-string[,] AssignGroup(int groups = 6)
+// Returns true if the Terminal was resized 
+bool TerminalResized()
 {
-    string[,] result = new string[groups, pettingZoo.Length / groups];
+    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
+}
 
-    int start = 0;
+// Displays random food at a random location
+void ShowFood()
+{
+    // Update food to a random index
+    food = random.Next(0, foods.Length);
 
-    for (int i = 0; i < groups; i++)
+    // Update food position to a random location
+    foodX = random.Next(0, width - player.Length);
+    foodY = random.Next(0, height - 1);
+
+    // Display the food at the location
+    Console.SetCursorPosition(foodX, foodY);
+    Console.Write(foods[food]);
+}
+
+// Changes the player to match the food consumed
+void ChangePlayer()
+{
+    player = states[food];
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+bool GotFood()
+{
+    return playerX == foodX && playerY == foodY;
+}
+
+// Temporarily stops the player from moving
+void FreezePlayer()
+{
+    System.Threading.Thread.Sleep(1000);
+    player = states[0];
+}
+
+// Reads directional input from the Console and moves the player
+void Move(int speed = 1)
+{
+    int lastX = playerX;
+    int lastY = playerY;
+
+    switch (Console.ReadKey(true).Key)
     {
-        for (int j = 0; j < result.GetLength(1); j++)
-        {
-            result[i, j] = pettingZoo[start++];
-        }
+        case ConsoleKey.UpArrow:
+            playerY--;
+            break;
+        case ConsoleKey.DownArrow:
+            playerY++;
+            break;
+        case ConsoleKey.LeftArrow:
+            playerX -= speed;
+            break;
+        case ConsoleKey.RightArrow:
+            playerX += speed;
+            break;
+        case ConsoleKey.Escape:
+            shouldExit = true;
+            break;
     }
 
+    // Clear the characters at the previous position
+    Console.SetCursorPosition(lastX, lastY);
+    for (int i = 0; i < player.Length; i++)
+    {
+        Console.Write(" ");
+    }
+
+    // Keep player position within the bounds of the Terminal window
+    playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
+    playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
+
+    // Draw the player at the new location
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Clears the console, displays the food and player
+void InitializeGame()
+{
+    Console.Clear();
+    ShowFood();
+    Console.SetCursorPosition(0, 0);
+    Console.Write(player);
+}
+
+string TypeOfCharacter()
+{
+    string result = "";
+    if (player == "(X_X)")
+    {
+        result = "freeze";
+    }
+    else if (player == "(^-^)")
+    {
+        result = "fast";
+    }
     return result;
-}
-
-void PrintGroup(string[,] group)
-{
-    for (int i = 0; i < group.GetLength(0); i++)
-    {
-        Console.Write($"Group {i + 1}: ");
-        for (int j = 0; j < group.GetLength(1); j++)
-        {
-            Console.Write($"{group[i, j]}  ");
-        }
-        Console.WriteLine();
-    }
-}
-
-void PlanSchoolVisit(string schoolName, int groups = 6)
-{
-    RandomizeAnimals();
-    string[,] group = AssignGroup(groups);
-    Console.WriteLine(schoolName);
-    PrintGroup(group);
 }
