ae6144e24e0cdf84e24d7ac7c34a064d458d0d2e
Tsotne Basiashvili <basiashvilic@gmail.com>
2024-08-03 07:25:16 +0400

    learned exception and try catch block in C#

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c78b4ed9a4629af5e4ee5a35d6ef14cda3a3ca96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3fd6ac1fbe2777460285de069f3cedff2d0083")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
