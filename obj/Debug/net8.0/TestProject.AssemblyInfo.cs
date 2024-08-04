7ae57b70712bf37c2928966c4bf1ba38cc5deb2e
Tsotne Basiashvili <basiashvilic@gmail.com>
2024-08-04 08:49:17 +0400

    learned throwing exceptions

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3fd6ac1fbe2777460285de069f3cedff2d0083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6144e24e0cdf84e24d7ac7c34a064d458d0d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
