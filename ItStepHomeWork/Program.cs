c14a3e9486420f9047184e9572dbf39babd28fde
tsotne basiashvili <basiashvilic@gmail.com>
2024-09-27 16:05:33 +0400

    added homework

---
+++ ItStepHomeWork/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+Calculator calc = new Calculator();
+
+Console.WriteLine(calc.Plus(10, 5.25));
+
+public class Calculator
+{
+    public double Plus(double a, double b)
+    {
+        return a + b;
+    }
+    public double Minus(double a, double b)
+    {
+        return a - b;
+    }
+    public double Multiplication(double a, double b)
+    {
+        return a * b;
+    }
+    public double Divide(double a, double b)
+    {
+        if (a == 0 || b == 0)
+        {
+            Console.WriteLine("Do not use 0");
+        }
+        return a / b;
+    }
+}
