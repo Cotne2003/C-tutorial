e1ecd701e5c8b35960d474b707672404b949d7a8
tsotne basiashvili <basiashvilic@gmail.com>
2024-10-16 20:34:48 +0400

    test

---
+++ DelegateBasicExample/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DelegateBasicExample
+{
+
+	internal class Program
+	{
+		delegate void LogDel(string text, DateTime dateTime);
+		static void Main(string[] args)
+		{
+			LogDel logDel = new LogDel(LogTextScreen);
+
+			logDel("text", DateTime.Now);
+		}
+		static void LogTextScreen(string text, DateTime dateTime)
+		{
+			Console.WriteLine($"{dateTime}: {text}");
+		}
+	}
+}
--- SchoolHRAdministration/Program.cs
+++ SchoolHRAdministration/Program.cs
@@ -1,98 +1,16 @@
-﻿using HRAdministrationApi;
-
-namespace SchoolHRAdministration
-{
-	public enum EmployeeType
+﻿namespace DelegateBasicExample
+{	internal class Program
 	{
-		Teacher,
-		HeadOfDepartment,
-		DeputyHeadMaster,
-		HeadMaster
-	}
-	internal class Program
-	{
+		delegate void LogDel(string text);
 		static void Main(string[] args)
 		{
-			decimal totalSalaries = 0;
+			LogDel logDel = new LogDel(LogTextScreen);
 
-			List<IEmployee> employess = new List<IEmployee>();
-			SeedData(employess);
-
-			totalSalaries = employess.Sum(e => e.Salary);
-
-			Console.WriteLine(totalSalaries);
-
-			Console.ReadKey();
+			logDel.Invoke("text");
 		}
-		public static void SeedData(List<IEmployee> employees)
+		static void LogTextScreen(string text)
 		{
-			IEmployee teacher1 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 1, "Bob", "Fisher", 40000);
-			employees.Add(teacher1);
-
-			IEmployee teacher2 = EmployeeFactory.GetEmployeeInstance(EmployeeType.Teacher, 2, "Jenny", "Thomas", 40000);
-			employees.Add(teacher2);
-
-
-			IEmployee headOfDepartment = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadOfDepartment, 3, "Brenda", "Mullins", 50000);
-			employees.Add(headOfDepartment);
-
-			IEmployee deputyHeadMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.DeputyHeadMaster, 4, "Devlin", "Brown", 60000);
-			employees.Add(deputyHeadMaster);
-
-
-			IEmployee headMaster = EmployeeFactory.GetEmployeeInstance(EmployeeType.HeadMaster, 5, "Damien", "Jones", 80000);
-			employees.Add(headMaster);
-		}
-	}
-
-	public class Teacher : EmployeeBase
-	{
-		public override decimal Salary { get => base.Salary + (base.Salary * 0.02m); }
-	}
-	public class HeadOfDepartment : EmployeeBase
-	{
-		public override decimal Salary { get => base.Salary + (base.Salary * 0.03m); }
-	}
-	public class  DeputyHeadMaster : EmployeeBase
-    {
-		public override decimal Salary { get => base.Salary + (base.Salary * 0.04m); }
-	}
-	public class HeadMaster : EmployeeBase
-	{
-		public override decimal Salary { get => base.Salary + (base.Salary * 0.05m); }
-	}
-	public static class EmployeeFactory
-	{
-		public static IEmployee GetEmployeeInstance(EmployeeType employeeType, int id, string firstName, string lastName, decimal salary)
-		{
-			IEmployee employee = null;
-			switch (employeeType)
-			{
-				case EmployeeType.Teacher:
-					employee = FactoryPattern<IEmployee, Teacher>.GetInstance();
-					break;
-				case EmployeeType.HeadOfDepartment:
-					employee = FactoryPattern<IEmployee, HeadOfDepartment>.GetInstance();
-					break;
-				case EmployeeType.DeputyHeadMaster:
-					employee = FactoryPattern<IEmployee, DeputyHeadMaster>.GetInstance();
-					break;
-				case EmployeeType.HeadMaster:
-					employee = FactoryPattern<IEmployee, HeadMaster>.GetInstance();
-					break;
-			}
-			if (employeeType != null)
-			{
-				employee.Id = id;
-				employee.FirstName = firstName;
-				employee.LastName = lastName;
-				employee.Salary = salary;
-			}
-			else
-			{
-				throw new NullReferenceException();
-			}
-			return employee;
+			Console.WriteLine($"{DateTime.Now}: {text}");
 		}
 	}
 }
