1581e18194e351e36079db7beb1ec843cf4c727c
tsotne basiashvili <basiashvilic@gmail.com>
2024-10-25 23:30:48 +0400

    did new HomeWork

--- ItStepHomeWork/Program.cs
+++ ItStepHomeWork/Program.cs
@@ -5,45 +5,95 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		Calculator calculator = new Calculator();
-		Condition condition = null;
-		condition = calculator.Minus; 
-		condition += calculator.Plus; 
-		condition += calculator.Division; 
-		condition += calculator.Multiplication;
-		condition += calculator.Remainder;
-		condition(10, 5);
+		CreditCard card = new CreditCard("Tsotne", "Basiashvili", "1089");
+		CreditCard card1 = new CreditCard("Adomel", "Arabuli", "1125");
+		// add balance
+		card.Deposit(1000);
+		Console.WriteLine(card.Balance);
+		// trasfer money
+		card.Transfer(500, card1);
+		// show information
+		card.DisplayInformation();
+		card1.DisplayInformation();
+		// change pin code
+		card.ChangePinCode();
 	}
 
-	delegate double Condition(double a, double b);
-
-	class Calculator
+	class CreditCard
 	{
-		public double Plus(double a, double b)
+		private Random random = new Random();
+		private string CardNumber { get => $"{random.Next(1000, 10000)}"; }
+        private string FirstName { get; set; }
+		private string LastName { get; set; }
+		private string FullName { get => $"{FirstName} {LastName}"; }
+		private string PinCode { get; set; }
+		public decimal Balance { get; set; }
+		private DateTime CardValidityPeriod { get; set; }
+		private decimal creditLimit = 5000;
+
+		public CreditCard(string firstName, string lastName, string pinCode)
+        {
+			CardValidityPeriod = DateTime.Now.AddYears(4);
+			FirstName = firstName;
+			LastName = lastName;
+			PinCode = pinCode;
+        }
+		public void Deposit(decimal amount)
 		{
-			Console.WriteLine($"{a} + {b} = {a + b}");
-			return a + b;
+			if (amount > 0 && Balance + amount <= creditLimit)
+			{
+				Balance += amount;
+			}
 		}
-		public double Minus(double a, double b)
+		public void Withdraw(decimal amount)
 		{
-			Console.WriteLine($"{a} - {b} = {a - b}");
-			return a - b;
+			if (amount > 0 && amount <= Balance)
+			{
+				Balance -= amount;
+			}
 		}
-		public double Division(double a, double b)
+		public void Transfer(decimal amount, CreditCard card)
 		{
-			Console.WriteLine($"{a} / {b} = {a / b}");
-			return a / b;
+			if (amount > 0 && amount <= Balance && card.Balance + amount <= card.creditLimit)
+			{
+				Balance -= amount;
+				card.Balance += amount;
+			}
 		}
-		public double Multiplication(double a, double b)
+		public void DisplayInformation()
 		{
-			Console.WriteLine($"{a} * {b} = {a * b}");
-			return a * b;
+			Console.WriteLine("Enter Pin Code:");
+			string pinInput = Console.ReadLine();
+			if (pinInput == PinCode)
+			{
+				Console.WriteLine($"Fullname: {FullName}\nBalance: {Balance}$\nCard Number: {CardNumber}\nCard Validity Period: {CardValidityPeriod}");
+			}
+			else
+			{
+				Console.WriteLine("Incorrect Pin Code");
+			}
 		}
-		public double Remainder(double a, double b)
+		public void ChangePinCode()
 		{
-			Console.WriteLine($"{a} % {b} = {a % b}");
-			return a % b;
+			Console.WriteLine("Enter Current Pin:");
+			string currentPin = Console.ReadLine();
+			if (currentPin == PinCode)
+			{
+				Console.WriteLine("Enter New Pin");
+				string newPin = Console.ReadLine();
+				if (newPin.Length != 0)
+				{
+					PinCode = newPin;
+				}
+				else
+				{
+					Console.WriteLine("Invalid Pin Code");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Incorrect Pin Code");
+			}
 		}
 	}
-
 }
--- ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
+++ ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8374afd6fcf5548f5fd2c11d5998853386a80830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a55aa80858833513315f1124cbd838d07f1ffb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
