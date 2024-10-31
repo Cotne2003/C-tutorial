b934983a02cada378273cec233b0e4ae4b11b275
tsotne basiashvili <basiashvilic@gmail.com>
2024-10-31 23:59:35 +0400

    added homework

--- ItStepHomeWork/Program.cs
+++ ItStepHomeWork/Program.cs
@@ -1,11 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-
-internal class Program
+﻿internal class Program
 {
 	static void Main(string[] args)
 	{
-		Func<string, string, bool> IsStringEqual;
-		Action<string, int> DisplayPerson = (str, age) => Console.WriteLine($"{str} {age}");
+		Birthday birthday = new Birthday();
+		BirthdayMember person1 = new BirthdayMember("Tsotne");
+		BirthdayMember person2 = new BirthdayMember("Giorgi");
+		BirthdayMember person3 = new BirthdayMember("Andria");
+		person1.Join(birthday);
+		person2.Join(birthday);
+		person3.Join(birthday);
+		birthday.SendInvite();
+	}
+	class MyEventArgs : EventArgs
+	{
+		public string InviteMessage = "the cake is being cut!";
+	}
+	class Birthday
+	{
+		public EventHandler<MyEventArgs> CakeCut;
+
+		protected virtual void OnCakeCut(MyEventArgs e)
+		{
+			CakeCut?.Invoke(this, e);
+		}
+		public void SendInvite()
+		{
+			MyEventArgs e = new MyEventArgs();
+			OnCakeCut(e);
+		}
+	}
+	class BirthdayMember
+	{
+		public string Name { get; set; }
+        public BirthdayMember(string name)
+        {
+            Name = name;
+        }
+        private void CakeCutHandler(object sender, MyEventArgs e)
+		{
+			Console.WriteLine($"Hi {Name}, Come to the second floor, {e.InviteMessage}");
+		}
+		public void Join(Birthday birthday)
+		{
+			birthday.CakeCut += CakeCutHandler;
+		}
 	}
 }
--- ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
+++ ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a55aa80858833513315f1124cbd838d07f1ffb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e20353613f7d5544a55779def1e33458c89581d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
