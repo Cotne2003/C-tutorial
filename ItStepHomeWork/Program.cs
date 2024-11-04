df67937e61a47636d467d96e620d6c0229d80ddd
tsotne basiashvili <basiashvilic@gmail.com>
2024-11-04 22:39:56 +0400

    homework

--- ItStepHomeWork/Program.cs
+++ ItStepHomeWork/Program.cs
@@ -1,48 +1,38 @@
-﻿internal class Program
+﻿namespace Linq
 {
-	static void Main(string[] args)
+	internal class Program
 	{
-		Birthday birthday = new Birthday();
-		BirthdayMember person1 = new BirthdayMember("Tsotne");
-		BirthdayMember person2 = new BirthdayMember("Giorgi");
-		BirthdayMember person3 = new BirthdayMember("Andria");
-		person1.Join(birthday);
-		person2.Join(birthday);
-		person3.Join(birthday);
-		birthday.SendInvite();
-	}
-	class MyEventArgs : EventArgs
-	{
-		public string InviteMessage = "the cake is being cut!";
-	}
-	class Birthday
-	{
-		public EventHandler<MyEventArgs> CakeCut;
+		static void Main(string[] args)
+		{
+			Person person1 = new Person(1, "Tsotne", 21, "Programmer");
+			Person person2 = new Person(2, "Giorgi", 25, "Philologist");
+			Person person3 = new Person(3, "Nikoloz", 19, "Scientist");
 
-		protected virtual void OnCakeCut(MyEventArgs e)
-		{
-			CakeCut?.Invoke(this, e);
+			List<Person> persons = new List<Person>();
+			persons.Add(person1);
+			persons.Add(person2);
+			persons.Add(person3);
+
+			List<Person> byAge = persons.OrderBy(a => a.Age).ToList();
+
+			foreach (Person person in byAge)
+			{
+				Console.WriteLine(person.Name + ": " + person.Age);
+			}
 		}
-		public void SendInvite()
+		class Person
 		{
-			MyEventArgs e = new MyEventArgs();
-			OnCakeCut(e);
-		}
-	}
-	class BirthdayMember
-	{
-		public string Name { get; set; }
-        public BirthdayMember(string name)
-        {
-            Name = name;
-        }
-        private void CakeCutHandler(object sender, MyEventArgs e)
-		{
-			Console.WriteLine($"Hi {Name}, Come to the second floor, {e.InviteMessage}");
-		}
-		public void Join(Birthday birthday)
-		{
-			birthday.CakeCut += CakeCutHandler;
+			private int Id { get; set; }
+			public string Name { get; set; }
+			public int Age { get; set; }
+			public string Department { get; set; }
+			public Person(int id, string name, int age, string department)
+			{
+				Id = id;
+				Name = name;
+				Age = age;
+				Department = department;
+			}
 		}
 	}
 }
--- ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
+++ ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b934983a02cada378273cec233b0e4ae4b11b275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2142450dc06ee821a2c244f84537c58446852b0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
