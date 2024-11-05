8321bbaff9f8fcd29eb4138840a1701cdb18ccc0
tsotne basiashvili <basiashvilic@gmail.com>
2024-11-05 06:31:54 +0400

    new homeWork

--- ItStepHomeWork/Program.cs
+++ ItStepHomeWork/Program.cs
@@ -1,38 +1,58 @@
-﻿namespace Linq
+﻿using System.Text.RegularExpressions;
+
+namespace Linq
 {
 	internal class Program
 	{
+		delegate int MyDelegate(int num1, int num2);
 		static void Main(string[] args)
 		{
-			Person person1 = new Person(1, "Tsotne", 21, "Programmer");
-			Person person2 = new Person(2, "Giorgi", 25, "Philologist");
-			Person person3 = new Person(3, "Nikoloz", 19, "Scientist");
+			#region
+			//Person person1 = new Person(1, "Tsotne", 21, "Programmer");
+			//Person person2 = new Person(2, "Giorgi", 25, "Philologist");
+			//Person person3 = new Person(3, "Nikoloz", 19, "Scientist");
 
-			List<Person> persons = new List<Person>();
-			persons.Add(person1);
-			persons.Add(person2);
-			persons.Add(person3);
+			//List<Person> persons = new List<Person>();
+			//persons.Add(person1);
+			//persons.Add(person2);
+			//persons.Add(person3);
 
-			List<Person> byAge = persons.OrderBy(a => a.Age).ToList();
+			//List<Person> byAge = persons.OrderBy(a => a.Age).ToList();
 
-			foreach (Person person in byAge)
+			//foreach (Person person in byAge)
+			//{
+			//	Console.WriteLine(person.Name + ": " + person.Age);
+			//}
+			#endregion
+
+			MyDelegate lambda = (int num1, int num2) => num1 + num2;
+			lambda += delegate (int num1, int num2)
 			{
-				Console.WriteLine(person.Name + ": " + person.Age);
-			}
+				return num1 + num2;
+			};
+			Console.WriteLine(lambda(1,2));
+
+			string url = "https://www.example.com/page";
+			string url2 = "http://www.example.com/page";
+			string urlPattern = @"^https:\/\/www\.[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(\/[a-zA-Z0-9-_.~%]*)*\/?$";
+			Console.WriteLine(Regex.IsMatch(url, urlPattern));
+			Console.WriteLine(Regex.IsMatch(url2, urlPattern));
 		}
-		class Person
-		{
-			private int Id { get; set; }
-			public string Name { get; set; }
-			public int Age { get; set; }
-			public string Department { get; set; }
-			public Person(int id, string name, int age, string department)
-			{
-				Id = id;
-				Name = name;
-				Age = age;
-				Department = department;
-			}
-		}
+		#region
+		//class Person
+		//{
+		//	private int Id { get; set; }
+		//	public string Name { get; set; }
+		//	public int Age { get; set; }
+		//	public string Department { get; set; }
+		//	public Person(int id, string name, int age, string department)
+		//	{
+		//		Id = id;
+		//		Name = name;
+		//		Age = age;
+		//		Department = department;
+		//	}
+		//}
+		#endregion
 	}
 }
--- ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
+++ ItStepHomeWork/obj/Debug/net8.0/ItStepHomeWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2142450dc06ee821a2c244f84537c58446852b0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df67937e61a47636d467d96e620d6c0229d80ddd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItStepHomeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
