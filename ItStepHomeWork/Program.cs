bef0ff1f65a1cf9d121fffccb82171cf7470b951
tsotne basiashvili <basiashvilic@gmail.com>
2024-11-28 12:17:21 +0400

    finished atm project

--- ItStepHomeWork/Program.cs
+++ ItStepHomeWork/Program.cs
@@ -7,10 +7,22 @@ namespace ATM
 		static void Main(string[] Args)
 		{
 			Console.WriteLine("Hello to ATM");
+			const string atmJsonPath = "C:\\Users\\Tsotne\\OneDrive\\Desktop\\ItStepAcademyProjects\\ATMApp\\DB\\atm.json";
+			string atmJsonContent = File.ReadAllText(atmJsonPath);
 			User userCard = new User();
-			Console.WriteLine(userCard.Name == null);
-			string leave = "";
-			while (leave != "no")
+			try
+			{
+				userCard = JsonSerializer.Deserialize<User>(atmJsonContent);
+				Console.WriteLine($"Welcome back {userCard.Name}! Press any key to start operations");
+				Console.ReadLine();
+			}
+			catch (System.Text.Json.JsonException e)
+			{
+				Console.WriteLine("Welcome to your first ATM account! Press any key to create account");
+				Console.ReadLine();
+			}
+			bool leave = false;
+			while (!leave)
 			{
 				if (userCard.Name == null)
 				{
@@ -34,32 +46,52 @@ namespace ATM
 				Console.WriteLine("3. Show Balance");
 				Console.WriteLine("4. Deposit");
 				Console.WriteLine("5. Withdraw");
+				Console.WriteLine("6. Change Pincode (Recommended if you haven't changed it yet)");
 				Console.WriteLine("7. Leave ATM");
 
 				if (int.TryParse(Console.ReadLine(), out int value))
 				{
+					decimal amount;
 					switch (value)
 					{
 						case 1:
-							Console.WriteLine($"Your account name is ${userCard.Name}");
+							Console.WriteLine($"Your account name is {userCard.Name}");
+							Console.WriteLine("Enter any key to return to menu");
+							Console.ReadLine();
 							break;
 						case 2:
-							Console.WriteLine($"Your account pin code is ${userCard.Pin}");
+							Console.WriteLine($"Your account pin code is {userCard.Pin}");
+							Console.WriteLine("Enter any key to return to menu");
+							Console.ReadLine();
 							break;
 						case 3:
-							Console.WriteLine($"Your balance is ${userCard.Balance}");
+							Console.WriteLine($"Your balance is {userCard.Balance}");
+							Console.WriteLine("Enter any key to return to menu");
+							Console.ReadLine();
 							break;
 						case 4:
-							while (decimal.TryParse(Console.ReadLine(), out decimal amount))
-							{
-								userCard.Deposit(amount);
-							}
+							Console.Write("Enter amount to deposit: ");
+							amount = decimal.Parse(Console.ReadLine());
+							userCard.Deposit(amount);
+							Console.WriteLine("Enter any key to return to menu");
+							Console.ReadLine();
 							break;
 						case 5:
-							while (decimal.TryParse(Console.ReadLine(), out decimal amount))
-							{
-								userCard.Withdraw(amount);
-							}
+							Console.Write("Enter amount to deposit: ");
+							amount = decimal.Parse(Console.ReadLine());
+							userCard.Withdraw(amount);
+							Console.WriteLine("Enter any key to return to menu");
+							Console.ReadLine();
+							break;
+						case 6:
+							Console.Write("Enter your new pin code: ");
+							string? newPin = Console.ReadLine();
+							userCard.ChangePin(newPin);
+							Console.WriteLine("Enter any key to return to menu");
+							Console.ReadLine();
+							break;
+						case 7:
+							leave = true;
 							break;
 						default:
 							Console.WriteLine("Invalid operation");
@@ -67,6 +99,7 @@ namespace ATM
 					}
 				}
 			}
+			File.WriteAllText(atmJsonPath, JsonSerializer.Serialize(userCard));
 		}
 
 		public class User
@@ -100,7 +133,7 @@ namespace ATM
 					Balance -= amount;
 					Console.WriteLine("The amount has been withdraw succesfully");
 				}
-				if (amount > Balance)
+				else if (amount > Balance)
 				{
 					Console.WriteLine("Not enought money to withdraw");
 				}
