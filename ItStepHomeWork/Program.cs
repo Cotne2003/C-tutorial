8dfc12e8327b5df2f983b16f0110c162db6cfbb0
tsotne basiashvili <basiashvilic@gmail.com>
2024-12-02 19:58:44 +0400

    coding drills

--- ItStepHomeWork/Program.cs
+++ ItStepHomeWork/Program.cs
@@ -4,14 +4,14 @@ namespace ATM
 {
 	internal class ATM
 	{
-
-
 		static void Main(string[] Args)
 		{
-			Console.WriteLine(NoSpace("asdasd  asd asd"));
-			string NoSpace(string input)
+
+			IEnumerable<string> GooseFilter(IEnumerable<string> birds)
 			{
-				return input.Replace(" ", "");
+				// return IEnumerable of string containing all of the strings in the input collection, except those that match strings in geese
+				string[] geese = new string[] { "African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher" };
+				return (IEnumerable<string>)birds.ToList().Where(b => !geese.Contains(b));
 			}
 		}
 	}
