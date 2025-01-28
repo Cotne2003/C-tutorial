39e6f6bf741ab327531276ad8b4222b376691ed6
tsotne basiashvili <basiashvilic@gmail.com>
2025-01-28 15:24:30 +0400

    added testing app

---
+++ AppForTesting/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForTesting
+{
+	public class Calculator
+	{
+		public double Percentage(double a, double b)
+		{
+			return b / 100 * a;
+		}
+	}
+}
