89a8c788deef8b74aca2f9366fa24b1ea24fca1d
tsotne basiashvili <basiashvilic@gmail.com>
2025-01-30 20:48:44 +0400

    added IRegister interface

---
+++ ClassWork/Program.cs
@@ -0,0 +1,42 @@
+
+using ClassWork.Interfaces;
+using ClassWork.Services;
+
+namespace ClassWork
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+			builder.Services.AddScoped<IRegister, RegisterUser>();
+			
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
+}
---
+++ ClassWork/Services/RegisterUser.cs
@@ -0,0 +1,20 @@
+﻿using ClassWork.Interfaces;
+
+namespace ClassWork.Services
+{
+	public class RegisterUser : IRegister
+	{
+		private readonly IRegister _register;
+
+		public RegisterUser(IRegister register)
+		{
+			_register = register;
+		}
+
+		public void RegisterUserOrProduct(string name, int id)
+		{
+			_register.RegisterUserOrProduct(name, id);
+			Console.WriteLine($"New Product - {name} by id - {id} is added.");
+		}
+	}
+}
